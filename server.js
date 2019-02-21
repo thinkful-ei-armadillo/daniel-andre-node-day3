c6c1c1f6c4f129b86ad1414089b72faf94711007
Daniel Ors <do7811@gmail.com>
2019-02-21 10:33:49 -0500

    completed starting files

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-/* eslint-disable quotes */
-"use strict";
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const MOVIES = require('./movies.json');
 
 const app = express();
 
+console.log(process.env.API_TOKEN);
+
 //uses here
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
 
-console.log(process.env.API_TOKEN);
+// validate API token from ENV and auth header
+app.use(function validateBearerToken(req, res, next) {
+
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  // move to the next middleware
+  next();
+});
 
 // build handler functions here
 
